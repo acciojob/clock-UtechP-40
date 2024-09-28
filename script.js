36f9a7130e321497d719b560ba251aad0047eb9e
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-29 00:24:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-//your JS code here. If required.
-let timer = document.querySelector("#timer");
-let i = 0;
-setInterval(()=>{
-    // i=i+1;
-    let date = new Date();
-    let zone;
-    if(date.getHours>=12){
-        zone = "PM"
-    }else{
-        zone = "AM"
-    }
-   
-    timer.innerText = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${zone}`;
-},1000)
+// Wrapping the code in an IIFE to avoid global scope pollution
+(function() {
+    const timer = document.querySelector("#timer");
+    let i = 0;
+
+    setInterval(() => {
+        const date = new Date();
+        let zone;
+        if(date.getHours() >= 12) {
+            zone = "PM";
+        } else {
+            zone = "AM";
+        }
+
+        // Format the hours for 12-hour clock
+        let hours = date.getHours() % 12;
+        hours = hours ? hours : 12; // If hours is 0, it should be 12
+
+        timer.innerText = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}, ${hours}:${date.getMinutes()}:${date.getSeconds()} ${zone}`;
+    }, 1000);
+})();
