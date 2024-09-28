039e5f581ac4fd0784de05aacf17e13017539251
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-29 00:21:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let timer = document.querySelector("#timer");
+let i = 0;
+setInterval(()=>{
+    // i=i+1;
+    let date = new Date();
+    let zone;
+    if(date.getHours>=12){
+        zone = "PM"
+    }else{
+        zone = "AM"
+    }
+   
+    timer.innerText = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${zone}`;
+},1000)
